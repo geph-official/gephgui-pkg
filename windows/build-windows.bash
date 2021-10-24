0f3efb2c7df2643c54b0d06300b70c2c002ffdb3
nullchinchilla <nullchinchilla@pm.me>
2021-10-24 14:38:20 -0500

    vendor in geph4-vpn-helper

--- windows/build-windows.bash
+++ windows/build-windows.bash
@@ -9,7 +9,7 @@ ISCC="./iscc/ISCC.exe"
 mkdir iscc
 unzip IS6.zip -d iscc
 cargo install --locked --path ../gephgui-wry
-cargo install --locked geph4-vpn-helper
+cargo install --locked --path ../geph4-vpn-helper
 
 curl https://f001.backblazeb2.com/file/geph-dl/geph4-binaries/$VERSION/geph4-client-windows-i386.exe > ../blobs/win-ia32/geph4-client.exe
 cp $(which gephgui-wry) ../blobs/win-ia32/
