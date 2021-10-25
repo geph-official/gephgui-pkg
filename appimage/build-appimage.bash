4c31c81c22a940e408fb3f0f87e6c41ad8ce7f71
nullchinchilla <nullchinchilla@pm.me>
2021-10-25 17:39:59 -0500

    ha

--- appimage/build-appimage.bash
+++ appimage/build-appimage.bash
@@ -1,16 +1,26 @@
 #!/bin/bash
-sudo apt-get -y install libgtk-3-dev libappindicator3-dev libwebkit2gtk-4.0-dev python3-pip curl 
+rm -rfv AppDir
+sudo apt-get -y install libgtk-3-dev libappindicator3-dev libwebkit2gtk-4.0-dev python3-pip curl patchelf strace
 sudo pip3 install appimage-builder
+sudo wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /usr/local/bin/appimagetool
+sudo wget https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1807-x86_64.AppImage -O /usr/local/bin/pkg2appimage
+sudo chmod +x /usr/local/bin/appimagetool
+sudo chmod +x /usr/local/bin/pkg2appimage
+
 
 mkdir -p AppDir/usr/local/bin
 (cd ../blobs/linux-x64 && sh ./pull-geph4-client.sh)
-cp ../blobs/linux-x64/geph4-client AppDir/usr/local/bin
-cp ../blobs/linux-x64/pac AppDir/usr/local/bin
-chmod +x AppDir/usr/local/bin/*
+#cp ../blobs/linux-x64/geph4-client AppDir/usr/local/bin
+#cp ../blobs/linux-x64/pac AppDir/usr/local/bin
+#chmod +x AppDir/usr/local/bin/*
 
 cd ../gephgui-wry
 # git submodule update --init --recursive
 
-CARGO_TARGET_DIR=target/appimage/ cargo build --release
-cp target/appimage/release/gephgui-wry ../appimage/AppDir/usr/local/bin
+rm -rfv target/appimage
+CARGO_TARGET_DIR=target/appimage/ cargo build
+cp target/appimage/debug/gephgui-wry ../appimage/
 
+cd ../appimage
+#appimage-builder --skip-test
+pkg2appimage appimage.yml
