d40e6faf81afb9f3b7103f56f37255feb86b4870
nullchinchilla <nullchinchilla@pm.me>
2021-10-25 22:28:57 -0500

    dockg

--- appimage/build-appimage.bash
+++ appimage/build-appimage.bash
@@ -2,11 +2,19 @@
 rm -rfv AppDir
 sudo apt-get update
 sudo apt-get -y install libgtk-3-dev libappindicator3-dev libwebkit2gtk-4.0-dev python3-pip curl patchelf strace libpango1.0-dev libgdk-pixbuf2.0-dev
-sudo pip3 install appimage-builder
-sudo wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /usr/local/bin/appimagetool
-sudo wget https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1807-x86_64.AppImage -O /usr/local/bin/pkg2appimage
-sudo chmod +x /usr/local/bin/appimagetool
-sudo chmod +x /usr/local/bin/pkg2appimage
+
+# Install appimagetool AppImage
+sudo wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /opt/appimagetool
+
+# workaround AppImage issues with Docker
+cd /opt/; sudo chmod +x appimagetool; sed -i 's|AI\x02|\x00\x00\x00|' appimagetool; sudo ./appimagetool --appimage-extract
+sudo mv /opt/squashfs-root /opt/appimagetool.AppDir
+sudo ln -s /opt/appimagetool.AppDir/AppRun /usr/local/bin/appimagetool
+
+sudo wget https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1807-x86_64.AppImage -O /opt/pkg2appimage
+cd /opt/; sudo chmod +x pkg2appimage; sed -i 's|AI\x02|\x00\x00\x00|' pkg2appimage; sudo ./pkg2appimage --appimage-extract
+sudo mv /opt/squashfs-root /opt/pkg2appimage.AppDir
+sudo ln -s /opt/pkg2appimage.AppDir/AppRun /usr/local/bin/pkg2appimage
 
 
 mkdir -p AppDir/usr/local/bin
@@ -24,4 +32,4 @@ cp target/appimage/release/gephgui-wry ../appimage/
 
 cd ../appimage
 #appimage-builder --skip-test
-APPIMAGE_EXTRACT_AND_RUN=1 pkg2appimage --appimage-extract-and-run appimage.yml
+pkg2appimage appimage.yml
