ecd393bfe5abf761fc9d14c37ee6a9ce501640ce
nullchinchilla <nullchinchilla@pm.me>
2021-10-25 22:05:14 -0500

    tesht

--- appimage/build-appimage.bash
+++ appimage/build-appimage.bash
@@ -24,4 +24,4 @@ cp target/appimage/release/gephgui-wry ../appimage/
 
 cd ../appimage
 #appimage-builder --skip-test
-pkg2appimage appimage.yml
+pkg2appimage --appimage-extract-and-run appimage.yml
