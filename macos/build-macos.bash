74c8437b6c4e04e7792ebbf74de2a8e82cf337a6
nullchinchilla <nullchinchilla@pm.me>
2021-11-17 14:37:26 -0600

    f

--- macos/build-macos.bash
+++ macos/build-macos.bash
@@ -2,3 +2,6 @@
 
 rsync -aW --delete template.app/ build.app/
 cargo install --locked --path ../gephgui-wry
+cp $(which gephgui-wry) build.app/Contents/MacOS/bin
+curl https://f001.backblazeb2.com/file/geph-dl/geph4-binaries/$(cat ../VERSION)/geph4-client-macos-amd64 > build.app/Contents/MacOS/bin/geph4-client
+chmod +x build.app/Contents/MacOS/bin/geph4-client
