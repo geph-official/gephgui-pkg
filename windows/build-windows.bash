9e35015ccacbf431afe8e037f9f194e8fb737e1f
nullchinchilla <nullchinchilla@pm.me>
2022-10-15 23:06:08 -0500

    fixg

--- windows/build-windows.bash
+++ windows/build-windows.bash
@@ -12,5 +12,5 @@ cargo install --locked --path ../gephgui-wry
 
 curl https://f001.backblazeb2.com/file/geph-dl/geph4-binaries/$VERSION/geph4-client-windows-i386.exe > ../blobs/win-ia32/geph4-client.exe
 cp $(which gephgui-wry) ../blobs/win-ia32/
-cp $(which geph4-vpn-helper) ../blobs/win-ia32/
+
 sh -c "$ISCC setup.iss"
