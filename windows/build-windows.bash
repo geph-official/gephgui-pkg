96cd6095c303b835a3433adbf590630f1f5ea35b
nullchinchilla <nullchinchilla@pm.me>
2023-01-08 12:10:44 -0500

    fix

--- windows/build-windows.bash
+++ windows/build-windows.bash
@@ -8,7 +8,7 @@ export VERSION=$(cat ../blobs/linux-x64/VERSION)
 ISCC="./iscc/ISCC.exe"
 mkdir iscc
 unzip IS6.zip -d iscc
-cargo install --locked --path ../gephgui-wry
+cargo install --features tray --locked --path ../gephgui-wry
 
 curl https://f001.backblazeb2.com/file/geph-dl/geph4-binaries/$VERSION/geph4-client-windows-i386.exe > ../blobs/win-ia32/geph4-client.exe
 cp $(which gephgui-wry) ../blobs/win-ia32/
