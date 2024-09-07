f8aedaa2ba48fd5d9b78edb7492b4784e9201506
nullchinchilla <nullchinchilla@pm.me>
2024-09-07 16:08:51 -0400

    Add x86_64-apple-darwin target for Rust, update macOS build and artifact naming for consistency

--- macos/build-macos.bash
+++ macos/build-macos.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rsync -aW --delete template.app/ build.app/
-cargo install --locked --path ../gephgui-wry
+cargo install --locked --target x86_64-apple-darwin --path ../gephgui-wry
 cp $(which gephgui-wry) build.app/Contents/MacOS/bin
 curl https://f001.backblazeb2.com/file/geph-dl/geph4-binaries/$(cat ../blobs/linux-x64/VERSION)/geph4-client-macos-amd64 > build.app/Contents/MacOS/bin/geph4-client
 chmod +x build.app/Contents/MacOS/bin/geph4-client
